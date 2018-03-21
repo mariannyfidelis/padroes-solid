3a8eab4ea8f1f6ee9c88f139e83d32ecdef04ef1
mariannyfidelis <mariannyfidelis@gmail.com>
2018-03-21 15:41:12 -0300

    Implementando o Observer com uma List<AcaoAposGerarNota>

---
+++ src/estudo/pratica/solid/observer/GeradorNotaFiscal.java
@@ -0,0 +1,29 @@
+package estudo.pratica.solid.observer;
+
+import java.util.List;
+
+public class GeradorNotaFiscal {
+
+	private final List<AcaoAposGerarNota> acoes;
+
+	public GeradorNotaFiscal(List<AcaoAposGerarNota> acoes) {
+		this.acoes = acoes;
+	}
+
+	public NotaFiscal gera(Fatura fatura) {
+
+		double valor = fatura.getValorMensal();
+
+		NotaFiscal nf = new NotaFiscal(valor, impostoSimplesSobreO(valor));
+
+		for (AcaoAposGerarNota acaoAposGerarNota : acoes) {
+			acaoAposGerarNota.executa(nf);
+		}
+
+		return nf;
+	}
+
+	private double impostoSimplesSobreO(double valor) {
+		return valor * 0.06;
+	}
+}
